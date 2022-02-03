9740fb9336aeac08220883a42a6bdefa50a1522e
Invidam <hansupark0208@gmail.com>
2022-02-03 14:00:33 +0900

    feat: Update Ch.11 Part. 9 ~ 10.
    2022.02.03.
    Summary Ch.11 9 ~ 10.

---
+++ src/ch-11/part-09/after-Score.js
@@ -0,0 +1,39 @@
+function score(candidate, medicalExam, scoringGuide) {
+  return new Score().execute(candidate, medicalExam, scoringGuide);
+}
+
+class Score {
+  constructor(candidate, medicalExam, scoringGuide) {
+    this._candidate = candidate;
+    this._medicalExam = medicalExam;
+    this._scoringGuide = scoringGuide;
+  }
+  execute() {
+    this._result = 0;
+    this._healthLevel = 0;
+    this._highMedicalRiskFlag = false;
+
+    this.scoreSmoking();
+
+    let certificationGrade = "regular";
+
+    if (
+      this._scoringGuide.stateWithLowCertification(this._candidate.originState)
+    ) {
+      certificationGrade = "low";
+      this._result -= 5;
+    }
+
+    // 비슷한 코드가 한참 이어짐
+    this._result -= Math.max(this._healthLevel - 5, 0);
+
+    return this._result;
+  }
+
+  scoreSmoking() {
+    if (this._medicalExam.isSmoker) {
+      this._healthLevel += 10;
+      this._highMedicalRiskFlag = true;
+    }
+  }
+}
---
+++ src/ch-11/part-10/after-Charge.js
@@ -0,0 +1,4 @@
+function chargeCalculator(customer, usage, provider) {
+  const baseCharge = customer.baseRate * usage;
+  return baseCharge * provider.connectionCharge;
+}
